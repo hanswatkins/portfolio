1b53e3067c1aaf425029c21b789ba4196e49e1f6
hanswatkins <hanswatkins@gmail.com>
2022-06-27 23:13:47 -0700

    adding event listeners

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log('Connected to script.js!')
 
+// queryselector
+const easterEgg = document.querySelector(".home-title");
+
+
+//function
+changeLogo = function () {
+    console.log('clicked!')
+}
+
+
+
+
+easterEgg.addEventListener("click", changeLogo)
